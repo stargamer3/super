9e1c96c0a11892c314f2261784e88b84e6dc0b91
stargamer3 <juliengrijalva@gmail.com>
2016-09-17 12:22:42 -0600

    domain

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+import sys
+from distutils.core import setup
+
+path, script = os.path.split(sys.argv[0])
+os.chdir(os.path.abspath(path))
+
+setup(name='domaincli',
+      version='0.0.11',
+      description='Register domains from the command line',
+      author='Greg Brockman',
+      author_email='gdb@gregbrockman.com',
+      url='http://domaincli.com',
+      scripts=['domaincli'],
+      install_requires='stripe'
+)
